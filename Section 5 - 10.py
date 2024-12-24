a9ce081b9fe87265b397de1d637295fea645d31d
A.I HUB <info.aihub17@gmail.com>
2024-12-24 07:09:36 -0800

    Add files via upload

--- Section 5 - 10.py
+++ Section 5 - 10.py
@@ -1,9 +1,9 @@
-# Line plot creation by passing 2 ndarrays with title, xlabel , ylabel ,marker , linestyle and color
+# mlc form
 import matplotlib.pyplot as myplt
 import numpy as mynp
 x_axis = mynp.arange(1,11)
 y_axis = x_axis**3
-myplt.plot(x_axis,y_axis,marker='o',linestyle='-.', color='g')
+myplt.plot(x_axis,y_axis,'o:')# mlc form
 myplt.title('Cube Function Line Plot')
 myplt.xlabel('X axis-Value -------')
 myplt.ylabel('Cube of Y axis-Value -------')
